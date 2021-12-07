e3b84b5502b71321d9850dcfa6c8bf51d5cf79d5
Lachlan Johnson <lachlanm.johnson@gmail.com>
2021-12-07 16:53:23 +1100

    Add home screen elements

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="mt-14 w-screen flex justify-center">
+      <div className="mt-24 text-center flex flex-col gap-5">
+        <h1 className="text-5xl font-bold">Grade Calculators</h1>
+        <span className="text-gray-800">What would you like to calculate?</span>
+        <div className="mt-5 flex justify-center gap-x-5 md:gap-x-9">
+          <Link
+            className="py-5 px-10 text-2xl rounded-full text-white hover:opacity-70 transition-opacity bg-pink-500"
+            to="/wam"
+          >
+            WAM
+          </Link>
+          <Link
+            className="py-5 px-10 text-2xl rounded-full text-white hover:opacity-70 transition-opacity bg-pink-500"
+            to="/gpa"
+          >
+            GPA
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
