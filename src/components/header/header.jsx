1d4634efc3ca8cbe27c1944c6a2e3178cf8a6e47
Lachlan Johnson <lachlanm.johnson@gmail.com>
2021-12-07 16:55:01 +1100

    Update 'Grade Calculator' to 'Grade Abacus'

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import Navbar from './navbar';
 const Header = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bg-purple-500 text-white h-14 flex items-center px-5 justify-between">
-      <h1>Grade Calculators</h1>
+      <h1>Grade Abacus</h1>
       <Navbar />
     </div>
   );
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="mt-14 w-screen flex justify-center">
       <div className="mt-24 text-center flex flex-col gap-5">
-        <h1 className="text-5xl font-bold">Grade Calculators</h1>
+        <h1 className="text-5xl font-bold">Grade Abacus</h1>
         <span className="text-gray-800">What would you like to calculate?</span>
         <div className="mt-5 flex justify-center gap-x-5 md:gap-x-9">
           <Link
